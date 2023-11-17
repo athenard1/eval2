a8a8b42251affac66757ebcb54dbe46f7c9e2fdb
athenard <anthoyd31@gmail.com>
2023-11-17 14:41:25 +0000

    Second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
-}
+  std::cout << "**********  Menu véhicule  **********" << std::endl;
+  std::cout << "1 - Multimédia" << std::endl;
+  std::cout << "2 - Téléphone" << std::endl;
+  std::cout << "3 - Connexions" << std::endl;
+  std::cout << "4 - Personnalisation - Configuration" << std::endl;
+  std::cout << "*************************************" << std::endl;
+int choix;
+std::cin >> choix;
+std::cout << choix << std::endl;
+std::cout << "choix de l'utilisateur :\n: " << choix << std::endl;
+  if (choix == 1) 
+  {
+    std::cout << "-Multimédia\n" ;
+  } 
+      if (choix == 2) 
+  {
+    std::cout << "-Téléphone\n" ;
+  } 
+      if (choix == 3) 
+  {
+    std::cout << "-Connexions\n" ;
+  }   
+      if (choix == 4) 
+  {
+    std::cout << "-Personnalisation - Configuration\n" ;
+  } 
+  else
+  { if (choix  > 4)
+    {
+      std::cout << "Choix non valide\n" ;
+    }
+      if (choix < 1)
+    {
+      std::cout << "Choix non valide\n" ;
+      }
+    }
+  }
