a6c689497e11ea712b0525cc834f9ec12f08d901
athenard1 <anthoyd31@gmail.com>
2023-12-15 14:00:27 +0000

    ajout du menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    
-  std::cout << "**********  Menu véhicule  **********" << std::endl;
-  std::cout << "1 - Multimédia" << std::endl;
-  std::cout << "2 - Téléphone" << std::endl;
-  std::cout << "3 - Connexions" << std::endl;
-  std::cout << "4 - Personnalisation - Configuration" << std::endl;
+int NombredePlacesdisponibles = 10; 
+
+  std::cout << NombredePlacesdisponibles << std::endl;
+
+int Choix; 
+  std::cin >> Choix;
+  std::cout << Choix << std::endl;
+
+  std::cout << "**********  Evaluation 2  **********" << std::endl;
+  std::cout << "1 - Entrée dans le parking" << std::endl;
+  std::cout << "2 - Nombre de places disponibles" << std::endl;
+  std::cout << "3 - Sortie du parking " << std::endl;
+  std::cout << "4 - Quitter " << std::endl;
   std::cout << "*************************************" << std::endl;
-int choix;
-std::cin >> choix;
-std::cout << choix << std::endl;
-std::cout << "choix de l'utilisateur :\n: " << choix << std::endl;
-  if (choix == 1) 
-  {
-    std::cout << "-Multimédia\n" ;
-  } 
-      if (choix == 2) 
-  {
-    std::cout << "-Téléphone\n" ;
-  } 
-      if (choix == 3) 
-  {
-    std::cout << "-Connexions\n" ;
-  }   
-      if (choix == 4) 
-  {
-    std::cout << "-Personnalisation - Configuration\n" ;
-  } 
-  else
-  { if (choix  > 4)
-    {
-      std::cout << "Choix non valide\n" ;
-    }
-      if (choix < 1)
-    {
-      std::cout << "Choix non valide\n" ;
-      }
-    }
-  }
+
+
+}
